ec6ac6f34166631a74eee5be2e40617c578947ab
josegabrielpadua <josegabrielpadua@gmail.com>
2024-03-03 20:00:50 -0300

    Adicionado outros arquivos html, um protótipo de fase final

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,11 @@
-from galeria.views import menu, shopee
+from galeria.views import menu, shopee, amazon, entrada, estoque, saida
 from django.urls import path
 
 urlpatterns = [
         path('', menu, name='menu'),
-        path('shopee/', shopee, name='shopee')
+        path('shopee/', shopee, name='shopee'),
+        path('amazon/', amazon, name='amazon'),
+        path('entrada/', entrada, name='entrada'),
+        path('estoque/', estoque, name='estoque'),
+        path('saída/', saida, name='saida')
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -6,3 +6,16 @@ def menu(request):
 
 def shopee(request):
     return render(request, 'shopee.html')
+
+def amazon(request):
+    return render(request, 'amazon.html')
+
+def entrada(request):
+    return render(request, 'entrada.html')
+
+def saida(request):
+    return render(request, 'saida.html')
+
+def estoque(request):
+    return render(request, 'estoque.html')
+
